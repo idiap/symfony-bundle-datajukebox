1a2acc6b6bbd93c548acce6462a5343251917e48
Cédric Dufour <cedric.dufour@ced-network.net>
2015-12-09 15:16:29 +0100

    - Allow to customize list/detail view templates

--- src/DataJukeboxBundle/TwigExtension.php
+++ src/DataJukeboxBundle/TwigExtension.php
@@ -38,7 +38,46 @@ namespace DataJukeboxBundle;
  */
 class TwigExtension
   extends \Twig_Extension
+  implements \Twig_Extension_InitRuntimeInterface
 {
+
+  /*
+   * PROPERTIES
+   ********************************************************************************/
+
+  /** Twig environment
+   *  @var \Twig_Environment
+   */
+  private $oTwigEnvironment;
+
+  /** Twig template (list view)
+   *  @var \Twig_TemplateInterface
+   */
+  private $oTwigTemplate_list;
+
+  /** Twig template (detail view)
+   *  @var \Twig_TemplateInterface
+   */
+  private $oTwigTemplate_detail;
+
+  /** Twig template (popup)
+   *  @var \Twig_TemplateInterface
+   */
+  private $oTwigTemplate_popup;
+
+
+  /*
+   * METHODS: Twig_Extension_InitRuntimeInterface
+   ********************************************************************************/
+
+  public function initRuntime(\Twig_Environment $oTwigEnvironment)
+  {
+    $this->oTwigEnvironment = $oTwigEnvironment;
+    $this->oTwigTemplate_list = $this->oTwigEnvironment->loadTemplate('DataJukeboxBundle:DataJukebox:list.html.twig');
+    $this->oTwigTemplate_detail = $this->oTwigEnvironment->loadTemplate('DataJukeboxBundle:DataJukebox:detail.html.twig');
+    $this->oTwigTemplate_popup = $this->oTwigEnvironment->loadTemplate('DataJukeboxBundle:DataJukebox:popup.html.twig');
+  }
+
 
   /*
    * METHODS: Twig_Extension
@@ -52,66 +91,64 @@ class TwigExtension
   public function getFunctions()
   {
     return array(
-      new \Twig_SimpleFunction(
-        'DataJukebox_list',
-        array($this, 'renderList'),
-        array('needs_environment' => true, 'is_safe' => array('html'))
-      ),
-      new \Twig_SimpleFunction(
-        'DataJukebox_detail',
-        array($this, 'renderDetail'),
-        array('needs_environment' => true, 'is_safe' => array('html'))
-      ),
-      new \Twig_SimpleFunction(
-        'DataJukebox_csv',
-        array($this, 'renderCSV'),
-        array('is_safe' => array('txt'))
-      ),
-      new \Twig_SimpleFunction(
-        'DataJukebox_xml',
-        array($this, 'renderXML'),
-        array('is_safe' => array('txt'))
-      ),
-      new \Twig_SimpleFunction(
-        'DataJukebox_json',
-        array($this, 'renderJSON'),
-        array('is_safe' => array('txt'))
-      ),
-      new \Twig_SimpleFunction(
-        'DataJukebox_popupContainer',
-        array($this, 'renderPopupContainer'),
-        array('needs_environment' => true, 'is_safe' => array('html'))
-      ),
+
+      // List view
+      new \Twig_SimpleFunction('DataJukebox_list', array($this, 'renderList'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listData', array($this, 'renderListData'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listHeader', array($this, 'renderListHeader'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listTitle', array($this, 'renderListTitle'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listDisplay', array($this, 'renderListDisplay'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listSearch', array($this, 'renderListSearch'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listLabel', array($this, 'renderListLabel'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listFilter', array($this, 'renderListFilter'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listRows', array($this, 'renderListRows'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listRow', array($this, 'renderListRow'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listFooter', array($this, 'renderListFooter'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listBrowser', array($this, 'renderListBrowser'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listActions', array($this, 'renderListActions'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listLinks', array($this, 'renderListLinks'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listHelp', array($this, 'renderListHelp'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listJavascript', array($this, 'renderListJavascript'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_listTemplate', array($this, 'initListTemplate'), array('is_safe' => array('html'))),
+
+      // Detail view
+      new \Twig_SimpleFunction('DataJukebox_detail', array($this, 'renderDetail'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_detailData', array($this, 'renderDetailData'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_detailHeader', array($this, 'renderDetailHeader'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_detailTitle', array($this, 'renderDetailTitle'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_detailDisplay', array($this, 'renderDetailDisplay'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_detailRow', array($this, 'renderDetailRow'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_detailFooter', array($this, 'renderDetailFooter'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_detailActions', array($this, 'renderDetailActions'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_detailLinks', array($this, 'renderDetailLinks'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_detailJavascript', array($this, 'renderDetailJavascript'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_detailTemplate', array($this, 'initDetailTemplate'), array('is_safe' => array('html'))),
+
+      // Popup
+      new \Twig_SimpleFunction('DataJukebox_popupContainer', array($this, 'renderPopupContainer'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('DataJukebox_popupTemplate', array($this, 'initPopupTemplate'), array('is_safe' => array('html'))),
+
+      // Export
+      new \Twig_SimpleFunction('DataJukebox_csv', array($this, 'renderCSV'), array('is_safe' => array('txt'))),
+      new \Twig_SimpleFunction('DataJukebox_xml', array($this, 'renderXML'), array('is_safe' => array('txt'))),
+      new \Twig_SimpleFunction('DataJukebox_json', array($this, 'renderJSON'), array('is_safe' => array('txt'))),
+
     );
   }
 
   public function getFilters()
   {
     return array(
-      new \Twig_SimpleFilter(
-        'DataJukebox_format',
-        array($this, 'formatHTML'),
-        array('is_safe' => array('html'))
-      ),
-      new \Twig_SimpleFilter(
-        'DataJukebox_formatCSV',
-        array($this, 'formatCSV'),
-        array('is_safe' => array('txt'))
-      ),
-      new \Twig_SimpleFilter(
-        'DataJukebox_formatXML',
-        array($this, 'formatXML'),
-        array('is_safe' => array('txt'))
-      ),
-      new \Twig_SimpleFilter(
-        'DataJukebox_formatJSON',
-        array($this, 'formatJSON'),
-        array('is_safe' => array('txt'))
-      ),
-      new \Twig_SimpleFilter(
-        'DataJukebox_map',
-        array($this, 'arrayMap')
-      ),
+
+      // Format
+      new \Twig_SimpleFilter('DataJukebox_format', array($this, 'formatHTML'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFilter('DataJukebox_formatCSV', array($this, 'formatCSV'), array('is_safe' => array('txt'))),
+      new \Twig_SimpleFilter('DataJukebox_formatXML', array($this, 'formatXML'), array('is_safe' => array('txt'))),
+      new \Twig_SimpleFilter('DataJukebox_formatJSON', array($this, 'formatJSON'), array('is_safe' => array('txt'))),
+
+      // Helpers
+      new \Twig_SimpleFilter('DataJukebox_map', array($this, 'arrayMap')),
+
     );
   }
 
@@ -120,23 +157,171 @@ class TwigExtension
    * METHODS
    ********************************************************************************/
 
-  public function renderList(\Twig_Environment $oTwigEnvironment, $data)
+  /*
+   * List view
+   */
+
+  public function renderList($data)
   {
-    $oTemplate = $oTwigEnvironment->loadTemplate('DataJukeboxBundle:DataJukebox:list.html.twig');
-    return $oTemplate->renderBlock('DataJukebox_list', $oTwigEnvironment->mergeGlobals(array('data' => $data)));
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_list', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
   }
 
-  public function renderDetail(\Twig_Environment $oTwigEnvironment, $data)
+  public function renderListData($data)
   {
-    $oTemplate = $oTwigEnvironment->loadTemplate('DataJukeboxBundle:DataJukebox:detail.html.twig');
-    return $oTemplate->renderBlock('DataJukebox_detail', $oTwigEnvironment->mergeGlobals(array('data' => $data)));
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listData', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
   }
 
-  public function renderPopupContainer(\Twig_Environment $oTwigEnvironment)
+  public function renderListHeader($data)
   {
-    $oTemplate = $oTwigEnvironment->loadTemplate('DataJukeboxBundle:DataJukebox:popup.html.twig');
-    return $oTemplate->renderBlock('DataJukebox_popupContainer', array());
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listHeader', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
   }
+
+  public function renderListTitle($data)
+  {
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listTitle', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderListDisplay($data)
+  {
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listDisplay', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderListSearch($data)
+  {
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listSearch', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderListLabel($data)
+  {
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listLabel', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderListFilter($data)
+  {
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listFilter', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderListRows($data)
+  {
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listRows', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderListRow($data, $row, $loop)
+  {
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listRow', $this->oTwigEnvironment->mergeGlobals(array('data' => $data, 'row' => $row, 'loop' => $loop)));
+  }
+
+  public function renderListFooter($data)
+  {
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listFooter', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderListBrowser($data)
+  {
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listBrowser', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderListActions($data)
+  {
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listActions', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderListLinks($data)
+  {
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listLinks', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderListHelp($data)
+  {
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listHelp', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderListJavascript($data)
+  {
+    return $this->oTwigTemplate_list->renderBlock('DataJukebox_listJavascript', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function initListTemplate($template)
+  {
+    $this->oTwigTemplate_list = $this->oTwigEnvironment->loadTemplate($template);
+  }
+
+  /*
+   * Detail view
+   */
+
+  public function renderDetail($data)
+  {
+    return $this->oTwigTemplate_detail->renderBlock('DataJukebox_detail', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderDetailData($data)
+  {
+    return $this->oTwigTemplate_detail->renderBlock('DataJukebox_detailData', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderDetailHeader($data)
+  {
+    return $this->oTwigTemplate_detail->renderBlock('DataJukebox_detailHeader', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderDetailTitle($data)
+  {
+    return $this->oTwigTemplate_detail->renderBlock('DataJukebox_detailTitle', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderDetailDisplay($data)
+  {
+    return $this->oTwigTemplate_detail->renderBlock('DataJukebox_detailDisplay', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderDetailRow($data)
+  {
+    return $this->oTwigTemplate_detail->renderBlock('DataJukebox_detailRow', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderDetailFooter($data)
+  {
+    return $this->oTwigTemplate_detail->renderBlock('DataJukebox_detailFooter', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderDetailActions($data)
+  {
+    return $this->oTwigTemplate_detail->renderBlock('DataJukebox_detailActions', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderDetailLinks($data)
+  {
+    return $this->oTwigTemplate_detail->renderBlock('DataJukebox_detailLinks', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function renderDetailJavascript($data)
+  {
+    return $this->oTwigTemplate_detail->renderBlock('DataJukebox_detailJavascript', $this->oTwigEnvironment->mergeGlobals(array('data' => $data)));
+  }
+
+  public function initDetailTemplate($template)
+  {
+    $this->oTwigTemplate_detail = $this->oTwigEnvironment->loadTemplate($template);
+  }
+
+  /*
+   * Popup
+   */
+
+  public function renderPopupContainer()
+  {
+    return $this->oTwigTemplate_popup->renderBlock('DataJukebox_popupContainer', array());
+  }
+
+  public function initPopupTemplate($template)
+  {
+    $this->oTwigTemplate_popup = $this->oTwigEnvironment->loadTemplate($template);
+  }
+
+  /*
+   * Export
+   */
 
   public function renderCSV($data)
   {
@@ -222,6 +407,10 @@ class TwigExtension
     return $sOutput;
   }
 
+  /*
+   * Format
+   */
+
   public function formatDateTime(\DateTime $oDateTime)
   {
     $sOutput = $oDateTime->format('Y-m-d H:i:s');
@@ -277,6 +466,10 @@ class TwigExtension
     if (is_resource($mValue)) return '"#RES#"';
     return sprintf('"%s"', addcslashes($mValue, "\\\"\f\n\r\t"));
   }
+
+  /*
+   * Helpers
+   */
 
   public function arrayMap($aMap,$aValues)
   {
